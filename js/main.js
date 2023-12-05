6d64b94a200eed22354fab56911e1b58834234e8
VILIAN <denk938@gmail.com>
2023-12-05 16:45:32 +0400

    modified:   css/style.css 	new file:   img/swiper_logo/acer.svg 	new file:   img/swiper_logo/apple.svg 	new file:   img/swiper_logo/bosch.svg 	new file:   img/swiper_logo/hp.svg 	new file:   img/swiper_logo/lenovo.svg 	new file:   img/swiper_logo/next.svg 	new file:   img/swiper_logo/sony.svg 	new file:   img/swiper_logo/sumsung.svg 	new file:   img/swiper_logo/viewsonic.svg 	new file:   js/main.js 	new file:   slider.html

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+  const resizableSwiper = (breakpoint, swiperClass, swiperSettings) => {
+    let swiper
+
+breakpoint = window.matchMedia(breakpoint)
+
+const enableSwiper = function (className, settings) {
+  swiper = new Swiper(className, settings)
+}
+
+const checker = function () {
+  if (breakpoint.matches) {
+    return enableSwiper(swiperClass, swiperSettings)
+  } else {
+    if (swiper !== undefined) swiper.destroy(true, true)
+    return
+  }
+}
+
+breakpoint.addEventListener('change', checker)
+checker()
+  }
+
+  resizableSwiper('(max-width: 767.5px)', '.image-slider', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  mousewheel: {
+      sensitivity: 1,
+      eventsTarget:".image-slider"
+  },
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  autoHeight: true,
+  })
+})
+
+const btn = document.querySelector(".swiper-button");
+const cards = document.querySelector(".image-slider__wrapper")
+
+btn.addEventListener('click',function(){
+  if (btn.innerHTML === "Показать всё") {
+    btn.innerHTML = "Скрыть";
+    cards.style.height = "auto";
+    } else {
+    btn.innerHTML = "Показать всё";
+    cards.style.height = "160px";
+    }
+})
+
+
+
